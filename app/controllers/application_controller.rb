55ae5b6b38d107efd940d43ab6a977a58443a4f1
Adrienne Yao <adrienne.yao@icloud.com>
2019-10-27 00:25:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,14 @@ class App < Sinatra::Base
     end
 
     post "/teams" do
+      
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       members = params[:team][:members]
+
       @super_heroes = members.collect do |member_params|
         SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
       end
+
       erb :team
     end
 
